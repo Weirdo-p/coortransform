05a1e00c82320f5e6529013f3c67714e60e5710a
weirdo <1047325464@qq.com>
2020-03-10 14:17:27 +0800

    build files

---
+++ build/CMakeFiles/a.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.dir/src/main.o"
+  "CMakeFiles/a.dir/src/utils.o"
+  "a.pdb"
+  "a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/coortrans.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coortrans.dir/src/main.o"
+  "CMakeFiles/coortrans.dir/src/utils.o"
+  "coortrans.pdb"
+  "coortrans"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coortrans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
