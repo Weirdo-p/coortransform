731b76c52a39ce8e1ffc363c867f575b85de379f
weirdo <1047325464@qq.com>
2020-03-12 11:17:50 +0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,13 @@
 #include "../include/utils.h"
+#include "../include/coortrans_4p.h"
 #include <iomanip>
 
 
 int main()
 {
-    
-    string path = "/home/xz/coding/coortransform/data/XYZ_origin_1.xyz";
-    vector31d data;
-    data = GetData(path);
-    for (auto i : data)
-    {
-        cout << setprecision(15) << i.transpose() << endl ;
-
-    }
-    cout << endl;
-    cout << setprecision(15) << data[0] * data[1].transpose() << endl;
-    return 0;
+   string pathA = "/home/xz/coding/coortransform/data/XYZ_origin_1.xyz";
+   string pathB = "/home/xz/coding/coortransform/data/XYZ_target_1.xyz";
+   string savepath = "/home/xz/coding/coortransform/data/4paramresult.txt";
+   const int n_4p = 4;
+   coortrans_4p trans_4p = coortrans_4p(pathA, pathB, n_4p, savepath);
 }
